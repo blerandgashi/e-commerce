bf3cf9b46a914403c9adaeebf96bc33acbc5c1db
Blerand Gashi <blerandgashi15@gmail.com>
2026-01-01 04:59:28 +0100

    done a bit of work on cart.js

---
+++ cart.js
@@ -0,0 +1,35 @@
+let cartItems = document.querySelector("#cart-items");
+const emptyCart = document.querySelector("#empty-cart");
+const cartSummary = document.querySelector("#cart-summary");
+
+function addCartItems(){
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+  if (cart.length === 0) {
+    emptyCart.style.display = "block";
+    cartSummary.style.display = "none";
+    cartItems.innerHTML = "";
+    return;
+  }
+
+  emptyCart.style.display = "none";
+  cartSummary.style.display = "block"
+
+  cart.forEach(product => {
+    cartItems.innerHTML += `
+      <div class="cart-item">
+        <img src="${product.image}" class="cart-item-image">
+        <div class="cart-item-details">
+          <h3 class="product-name">${product.name}</h3>
+          <p class="cart-item-price">${product.price}</p>
+          <div class="quantity-controls">
+            <button class="quantity-btn decrease-btn" data-id="${product.id}">-</button>
+            <span class="quantity">${product.quantity}</span>
+            <button class="quantity-btn increase-btn">+</button>
+          </div>
+        </div>
+      </div>
+    `
+  })
+}
+addCartItems()
